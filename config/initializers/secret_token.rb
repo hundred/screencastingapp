4194c96b23f38af4072eee471d5ec393889979b2
hundred <chiska86@aol.com>
2012-10-22 00:13:44 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccc::Application.config.secret_token = 'c60ce9fd4658f90fe272c54a77ee4f10a785c4ed4b1c52d672b7c2189b2a3d95c2b60000406785fb866f0bab1188c923f82f6e32b9f16e5245e244d2607d0ab5'
---
+++ db/migrate/20121021230256_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :task
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
